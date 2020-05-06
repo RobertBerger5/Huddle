8475d63e5f4f83067ed4bd9612939986a50efef2
ckieras <kieras1@stolaf.edu>
2020-05-06 14:51:43 -0700

    Overlayed Icons on Cards

--- MCAProj/components/Card.js
+++ MCAProj/components/Card.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     width: Layout.window.width - 30,
     height: Layout.window.height - BOTTOM_BAR_HEIGHT * 6,
     borderRadius: 20,
-    overflow: 'hidden', // this does magic
+    overflow: 'hidden',
   },
   title: {
     position: 'absolute',
