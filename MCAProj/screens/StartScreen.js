889c420f9a143319090d5849544b8b8c0b41b961
Robert Berger <robertberger5@gmail.com>
2020-05-31 13:19:01 -0500

    fix ip address to point at our cloudflare server

--- MCAProj/screens/StartScreen.js
+++ MCAProj/screens/StartScreen.js
@@ -14,7 +14,7 @@ import CheckBox from 'react-native-check-box';
 //'http://173.28.80.230:3000'
 //official server: 'http://161.35.54.15:3000'
 
-const serverip = 'http://173.28.80.230:3000';
+const serverip = 'http://161.35.54.15:3000';
 
 var {height, width} = Dimensions.get('window');
 
