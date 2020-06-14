8983a96fed71374c14e234f51999609d34bddf4d
Rob Berger <robertberger5@gmail.com>
2020-06-14 15:29:01 -0500

    show advertisements on start screen, also lint a lot of the code

--- MCAProj/screens/StartScreen.js
+++ MCAProj/screens/StartScreen.js
@@ -1,13 +1,13 @@
 //this is the main/starting page when opening our app
 import React from 'react';
-import { StyleSheet, TouchableOpacity, Button, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native';
+import { StyleSheet, TouchableOpacity, Button, Text, View, Dimensions, Image, Animated, PanResponder, Platform } from 'react-native';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Dropdown } from 'react-native-material-dropdown';
 import socketIO from 'socket.io-client';
 import Icon from 'react-native-vector-icons/Entypo';
 import Modal from 'react-native-modal';
 import CheckBox from 'react-native-check-box';
-import { AdMobBanner } from 'expo-ads-admob';
+import { AdMobBanner, AdMobInterstitial } from 'expo-ads-admob';
 
 //Our IP's
 //'http://65.128.45.107:3000'
@@ -17,151 +17,226 @@ import { AdMobBanner } from 'expo-ads-admob';
 
 const serverip = 'http://161.35.54.15:3000';
 
-var {height, width} = Dimensions.get('window');
+var { height, width } = Dimensions.get('window');
 
+var bannerAdUnitID = null;
+var intAdUnitID = null;
+
+async function showAd() {
+  await AdMobInterstitial.setAdUnitID(intAdUnitID); // Test ID, Replace with your-admob-unit-id
+  await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true });
+  await AdMobInterstitial.showAdAsync();
+}
 
 class StartScreen extends React.Component {
-    //Default constructor
-    constructor(props) {
-      super(props);
-      this.socket = socketIO(serverip, {
-        //query:'pass=password',
-        transports: ['websocket'], jsonp: false
-      });
+  //Default constructor
+  constructor(props) {
+    super(props);
+    this.socket = socketIO(serverip, {
+      //query:'pass=password',
+      transports: ['websocket'], jsonp: false
+    });
 
-      this.socket.connect();
-      console.log('connect!');
+    this.socket.connect();
 
-      this.socket.on('connect', () => {
-        console.log('connected to socket server');
-      });
+    this.socket.on('connect', () => {
+      console.log('connected to socket server');
+    });
 
-      this.socket.on('disconnect', () => {
-        console.log('connection to server lost');
-        alert("Lost connection to server");
-      });
+    this.socket.on('disconnect', () => {
+      console.log('connection to server lost');
+      alert("Lost connection to server");
+    });
 
-      this.socket.on('user_err',(msg)=>{
-        alert(msg);
-      })
+    this.socket.on('user_err', (msg) => {
+      alert(msg);
+    })
 
-      this.state ={
-        isModalVisible: false,
-        isChecked: false,
-        cardnum: '50', 
-      }
-
-      global.index = 0;
-
+    this.state = {
+      isModalVisible: false,
+      isChecked: false,
+      cardnum: '50',
+      adShowing: true,
     }
 
-  joinFunc = () => {
-   navigation.navigate('Join')
+    global.index = 0;
+
+    if (true) {
+      //test ads, always use unless we're in production
+      bannerAdUnitID = "ca-app-pub-3940256099942544/6300978111";
+      intAdUnitID = "ca-app-pub-3940256099942544/8691691433";
+    } else if (Platform.OS === 'android') {
+      //TODO: carefully test if android ads load
+      bannerAdUnitID = "ca-app-pub-3420284063429373/7355123212";
+      intAdUnitID = "ca-app-pub-3420284063429373/6965569969";
+    } else if (Platform.OS === 'ios') {
+      //TODO: carefully test if ios ads load
+      bannerAdUnitID = "ca-app-pub-3420284063429373/3409468336";
+      intAdUnitID = "ca-app-pub-3420284063429373/7157141655";
+    }
   }
 
-toggleModal = () => {
-  var bool = this.state.isModalVisible;
-  this.setState({isModalVisible: !bool});
+  componentDidMount() {
+    //after requestAdAsync
+    AdMobInterstitial.addEventListener("interstitialDidLoad", () => {
+      //console.log("interstitialDidLoad")
+    });
+    //after requestAdAsync
+    AdMobInterstitial.addEventListener("interstitialDidFailToLoad", () => {
+      //console.log("interstitialDidFailToLoad")
+      console.log("Video Ad Error");
+      this.setState({ adShowing: false });
+    });
+    //after showAdAsync
+    AdMobInterstitial.addEventListener("interstitialDidOpen", () => {
+      //console.log("interstitialDidOpen")
+    });
+    //user closed the ad
+    AdMobInterstitial.addEventListener("interstitialDidClose", () => {
+      //console.log("interstitialDidClose")
+      this.setState({ adShowing: false });
+    });
+
+    AdMobInterstitial.addEventListener("interstitialWillLeaveApplication", () => {
+      //console.log("interstitialWillLeaveApplication")
+    });
+  }
+  componentWillUnmount() {
+    AdMobInterstitial.removeAllListeners();
+  }
+
+  joinFunc = () => {
+    navigation.navigate('Join')
+  }
+
+  toggleModal = () => {
+    var bool = this.state.isModalVisible;
+    this.setState({ isModalVisible: !bool });
   };
 
-  bannerError(){
-    alert("Banner Error");
+  bannerError() {
+    console.log("Banner Error");
     return;
   }
 
 
 
-render() {
+  renderMainContent() {
 
-//Data values for the settings dropdown
-  let cards = [{
-    value: '10',
-  },{
-    value: '20',
-  }, {
-    value: '30',
-  }, {
-    value: '40',
-  },{
-    value: '50',
-  }];
+    //TODO: only show when page is navigated to, and not every time it renders
+    if (this.state.adShowing) {
+      showAd();
+    }
 
     return (
-      <View style={{ flex: 1, backgroundColor: '#fdf6f2', justifyContent: 'center',
-      alignItems: 'center',}}>
+      <View style={{
+        flex: 1, backgroundColor: '#fdf6f2', justifyContent: 'center',
+        alignItems: 'center',
+      }}>
 
-      <View style={{ flex: 1, marginTop: height * 0.08, paddingLeft: width * 0.80, marginRight: 5}}>
-         {/* Create the icon and modal pannel for the settings */}
+        <View style={{ flex: 1, marginTop: height * 0.08, paddingLeft: width * 0.80, marginRight: 5 }}>
+          {/* Create the icon and modal pannel for the settings */}
           <Icon
-          name="cog"
-          color='#a78d8a'
-          size={35}
-         onPress={this.toggleModal}></Icon>
-        <Modal
-        isVisible={this.state.isModalVisible}
-        deviceWidth={width}
-        deviceHeight={height}
-        color = 'white'
-        >
-          {/* View to define the settings modal */}
-          <View style={{flex: 1, margin: 20, justifyContent: 'center'}}>
-            <View style={{backgroundColor: '#fdf6f2', borderRadius: 12, padding: 20}}>
-            <View style={{ flexDirection: 'row', justifyContent: 'center',  alignItems: 'center'}}>
-            <Text style={{color: '#a78d8a', fontSize: RFPercentage(3)}}>Wheelchair Accessible</Text>
-            <CheckBox
-              style={{flex: 1, padding: 10, margin: 10}}
-              onClick={()=>{this.setState({isChecked:!this.state.isChecked})}}
-              isChecked={this.state.isChecked}
-              />
+            name="cog"
+            color='#a78d8a'
+            size={35}
+            onPress={this.toggleModal}></Icon>
+          <Modal
+            isVisible={this.state.isModalVisible}
+            deviceWidth={width}
+            deviceHeight={height}
+            color='white'
+          >
+            {/* View to define the settings modal */}
+            <View style={{ flex: 1, margin: 20, justifyContent: 'center' }}>
+              <View style={{ backgroundColor: '#fdf6f2', borderRadius: 12, padding: 20 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                  <Text style={{ color: '#a78d8a', fontSize: RFPercentage(3) }}>Wheelchair Accessible</Text>
+                  <CheckBox
+                    style={{ flex: 1, padding: 10, margin: 10 }}
+                    onClick={() => { this.setState({ isChecked: !this.state.isChecked }) }}
+                    isChecked={this.state.isChecked}
+                  />
+                </View>
+                <Dropdown
+                  style={{ marginBottom: 20 }}
+                  label='Max Restaurants Shown'
+                  data={[{
+                    value: '10',
+                  }, {
+                    value: '20',
+                  }, {
+                    value: '30',
+                  }, {
+                    value: '40',
+                  }, {
+                    value: '50',
+                  }]}
+                  defaultValue='10'
+                  onChangeText={cardnum => this.setState({ cardnum })}
+                />
+                <Button title="Close" onPress={this.toggleModal} color='#e18a7a' />
               </View>
-              <Dropdown
-                style={{marginBottom: 20}}
-                label='Max Restaurants Shown'
-                data={cards}
-                defaultValue='10'
-                onChangeText = {cardnum => this.setState({cardnum})}
-              />
-            <Button title="Close" onPress={this.toggleModal} color='#e18a7a' />
             </View>
-          </View>
-        </Modal>
+          </Modal>
 
-      </View>
-      <View style={{ flex: 1.5, justifyContent: 'flex-end'}}>
-        <Text style={styles.appName}>Convenir</Text>
-      </View>
+        </View>
+        <View style={{ flex: 1.5, justifyContent: 'flex-end' }}>
+          <Text style={styles.appName}>Convenir</Text>
+        </View>
 
-      <View style={{ flex: 3.5, justifyContent: 'flex-start'}}>
-        {/* Start Game button */}
-        <TouchableOpacity
-          style={styles.btn}
-          onPress =  {() => this.props.navigation.navigate('Filter', {socket: this.socket, cardnum: this.state.cardnum, access: this.state.isChecked})}>
-          <Text style={{fontWeight: 'bold',  fontSize: 20}}>Create Room</Text>
-        </TouchableOpacity>
+        <View style={{ flex: 3.5, justifyContent: 'flex-start' }}>
+          {/* Start Game button */}
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => this.props.navigation.navigate('Filter', { socket: this.socket, cardnum: this.state.cardnum, access: this.state.isChecked })}>
+            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Create Room</Text>
+          </TouchableOpacity>
 
-        {/* Join game button */}
-        <TouchableOpacity
-          style={styles.btn}
-          onPress =  {() =>  this.props.navigation.navigate('Join', {socket: this.socket})}>
-          <Text style={{fontWeight: 'bold', fontSize: 20}}>Join Room</Text>
-        </TouchableOpacity>
+          {/* Join game button */}
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => this.props.navigation.navigate('Join', { socket: this.socket })}>
+            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Join Room</Text>
+          </TouchableOpacity>
 
-      </View>
+        </View>
 
-      <View style={{flex:1, justifyContent:'flex-end'}}>
-      <AdMobBanner
-          style={{width: "100%"}}
-          bannerSize="fullBanner"
-          adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID for now
-          //android ad-unit-id: ca-app-pub-3420284063429373/7355123212
-          //ios ad-unit-id: ca-app-pub-3420284063429373/3409468336
-          servePersonalizedAds // true or false
-          onDidFailToReceiveAdWithError={this.bannerError} />
-      </View>
+        {/*<View style={{flex:2,justifyContent:'flex-end'}}>
+          <Button
+          title="(ad test)"
+          onPress={showAd}
+          />
+        </View>*/}
+
+        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+          <AdMobBanner
+            style={{ width: "100%" }}
+            adUnitID={bannerAdUnitID}
+            servePersonalizedAds // true or false
+            onDidFailToReceiveAdWithError={this.bannerError} />
+        </View>
 
       </View>
 
     );
+  }
+
+  render() {
+    //TODO: only show when page is navigated to, and not every time it renders
+    if (this.state.adShowing) {
+      showAd(); //this.state.adShowing should turn to false either when the user closes the ad, of if the ad fails to load at all
+      return (
+        <View style={{
+          flex: 1, backgroundColor: '#fdf6f2', justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <Text  style={{ fontWeight: 'bold', fontSize: 20 }}>Loading Advertisement...</Text>
+        </View>
+      )
+    } else {
+      return this.renderMainContent();
+    }
   }
 }
 
@@ -178,7 +253,7 @@ const styles = StyleSheet.create({
     color: '#a78d8a',
     overflow: 'hidden',
     padding: 20,
-    textAlign:'center',
+    textAlign: 'center',
     margin: 20,
   }
 });
